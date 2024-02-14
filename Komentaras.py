9a37e7d55a7e59db0d9c09d74c9de0e6b4974387
TFS33 <152039661+TFS33@users.noreply.github.com>
2024-02-15 00:07:15 +0200

    Add files via upload

---
+++ Komentaras.py
@@ -0,0 +1,13 @@
+#kviečiu_nežinomą_funkciją()
+
+def atitraukimo_pavyzdys():
+#     print("pradzia!")
+    vaisiai = ["obuolys", "kriaušė", "slyva"]
+    for vaisius in vaisiai:
+        print(vaisius)
+        if vaisius == "kriaušė":
+            break
+#     print("pabaiga!")
+
+#kviečiu_kitą_funkciją()
+atitraukimo_pavyzdys()
