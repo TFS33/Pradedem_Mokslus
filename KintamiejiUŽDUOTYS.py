7137aea9e9ef5cfcb656454bf7d19f606a9df0af
TFS33 <152039661+TFS33@users.noreply.github.com>
2024-02-20 19:55:23 +0200

    Add files via upload

---
+++ KintamiejiUŽDUOTYS.py
@@ -0,0 +1,30 @@
+vardas = "Tomas"
+print("Mano vardas yra..."+ vardas)
+print()
+Akademine_grupe = "ifzm-6"
+Vidurkis = "8"
+print("""Akademine grupė: """ + Akademine_grupe)
+print("""Vidurkis: """ + Vidurkis)
+print()
+print()
+skanduote = """Lietuva! """
+print(skanduote * 3)
+print()
+#valstybes duomenys
+pavadinimas  = "Praga"
+valstybe = "Čekija"
+ikurimo_data = "IX a."
+platuma = "50°05°00° š."
+ilguma = "14°25°00° r."
+plotas = "496 km²"
+gyventoju_skaicius_tukstanciais = "1309"
+print("""pavadinimas """ + pavadinimas)
+print("""valstybe """ + valstybe)
+print("""ikurimo data """ + ikurimo_data)
+print("""platuma """ + platuma)
+print("""ilguma """ + ilguma)
+print("""plotas """ + plotas)
+print("""gyventoju skaicius tukstancais """ + gyventoju_skaicius_tukstanciais) 
+print()
+zodis = "bimbam! "
+print(zodis * 5)
