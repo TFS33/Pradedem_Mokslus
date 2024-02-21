500074c278f7aaad4451d5bd53a10ec0ae21cf71
TFS33 <152039661+TFS33@users.noreply.github.com>
2024-02-21 18:08:10 +0200

    Add files via upload

---
+++ Kintamieji.py
@@ -0,0 +1,39 @@
+tekstas = "miegojau , miegu ir mieguosiu"
+print(tekstas)
+vardas = "Sai"
+pavardė = "Rea"
+print(vardas +  pavardė)
+skaičius_1 = 33
+skaičius_2 = 66
+print(skaičius_1)
+print(skaičius_2)
+print(skaičius_1 + skaičius_2)
+marke = "Audi"
+modelis = "80"
+variklis = "1.6L.benzinas"
+metai = "1987"
+technikine = True
+draudimas = False
+print(marke)
+print(modelis)
+print(variklis)
+print(metai)
+print(technikine)
+print(draudimas)
+print("marke", marke)
+print("modelis", modelis)
+print("variklis", variklis)
+print("metai", metai)
+print("technikine", technikine)
+print("draudimas", draudimas)
+print("marke", type(marke))
+print("modelis", type(modelis))
+print("variklis", type(variklis))
+print("metai", type(metai))
+print("technikine", type(technikine))
+print("draudimas", type(draudimas))
+#uzduotys
+
+
+
+
---
+++ skaiciai_math_uzduotys.py
@@ -0,0 +1,26 @@
+x = 5
+y = 2
+z = x + y
+print(x)
+print(y)
+print(z)
+print()
+print()
+a = 2
+b = 5
+c = 9
+suma = a + b + c
+skirtumas = a - b - c
+sandauga = a * b * c
+dalmenis = a / b / c
+print("""2 + 5 + 9 = """ , suma)
+print("""2 - 5 - 9 = """ , skirtumas)
+print("""2 * 5 * 9 = """ , sandauga)
+print("""2 / 5 / 9 = """ , dalmenis)
+print()
+print()
+q = 4
+w = 5
+print("pradinis", q , "galutinis" , q + 5 )
+print("pradinis" , w , "galutinis" , w * 2)
+exit
